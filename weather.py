17cf6fec0eb36cff7316e1524b12dc01e1d46d2b
Adamh25 <162492139+Adamh25@users.noreply.github.com>
2024-03-07 09:49:34 +0000

    Create weather.py
    contains the WeatherAPI class, which obtains weather data from OpenWeatherMap.
    It makes HTTP requests to the OpenWeatherMap API endpoint and analyzes the JSON response to extract the relevant weather info

---
+++ weather.py
@@ -0,0 +1,16 @@
+# weather.py
+import requests
+
+class WeatherAPI:
+    def __init__(self):
+        self.api_key = 'your_api_key' 
+      #e.g. e6df251613c11107a906aa5ffa18e1ac
+
+    def fetch_weather_data(self, city):
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={self.api_key}&units=metric'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Failed to fetch weather data.")
+            return None
